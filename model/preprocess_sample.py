Updates on the install process for tensorflow/cuda, fixing the seed error in train_model and updating the normalization for the binary mask images.

model/preprocess_sample.py
@@ -71,7 +71,7 @@ def preprocess_xy_images(image_list, mask_list, size=(576, 576)):
 
     # Normalize
     x = np.asarray(x, dtype=np.float32) / 255.0
-    y = np.asarray(y, dtype=np.float32) / 1.0
+    y = np.asarray(y, dtype=np.float32) / np.max(y)
 
     # Reshape X to (n_examples, size, size, 3[RGB])
     x = x.reshape(x.shape[0], x.shape[1], x.shape[2], 3)

model/train_model.py
@@ -2,8 +2,8 @@ import glob
 # import argparse
 #
 # import tensorflow as tf
-# # gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
-# # session = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
+# gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
+# session = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from keras.callbacks import ModelCheckpoint, CSVLogger
@@ -71,6 +71,7 @@ inputpath = "c:\\nycdata\\sample_dataset_tiles\\"
 maskpath = "c:\\nycdata\\sample_dataset_mask_tiles\\"
 weightfile = "c:\\nycdata\\test_weights.h5"
 logfile = "c:\\nycdata\\test_log.csv"
+
 def main():
     # parser = argparse.ArgumentParser(
     #     formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -112,6 +113,7 @@ def main():
     train_gen = get_augmented(
         x_train,
         y_train,
+        seed=seed,
         batch_size=4, #2
         data_gen_args=dict(
             rotation_range=30.,