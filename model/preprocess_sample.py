0f0246e5c9482728418b46c3c74fd87ddac75f8d
Joe Ranalli <jar339@psu.edu>
2022-10-20 13:13:55 -0400

    Add tool for splitting train and test datasets

--- model/preprocess_sample.py
+++ model/preprocess_sample.py
@@ -1,9 +1,12 @@
+import os
+import shutil
+
+import numpy as np
 from utils.generate_blank_json import generate_blank_json_dir
 from utils.json_to_dataset import json_to_binary
 from utils.slice_dataset_tiles import slice_image
-from utils.fileio import files_of_type
+from utils.fileio import files_of_type, verify_dir, is_dir_empty, clear_dir
 from utils.delete_blanks import delete_blank_tiles
-
 
 def preprocess_files():
     mydir = "c:\\nycdata\\sample_dataset\\"
@@ -71,3 +74,74 @@ def preprocess_xy_images(image_list, mask_list, size=(576, 576)):
     y = y.reshape(y.shape[0], y.shape[1], y.shape[2], 1)
 
     return x, y
+
+
+def split_test_train(image_dir, mask_dir, output_root, test_ratio=0.1,
+                     seed=None, overwrite=False, imext="png"):
+    """
+    Separate a test and training dataset.
+
+    Parameters
+    ----------
+    image_dir: str
+        Full path to directory containing the images
+    mask_dir: str
+        Full path to directory containing masks (must match filenames in
+        image_dir.
+    output_root: str
+        Root path to directory where files will be output. Directory names
+        coming out will be: test_imgs_SEED, test_masks_SEED, train_imgs_SEED,
+        train_masks_SEED
+    test_ratio: float (default 0.1)
+        Fraction of images that should be made the test data
+    seed: int (default None)
+        Random seed for the random number generator. If None, no seed
+        will be used. Caution! This will affect the global
+        numpy.random.seed()!
+    overwrite: bool (default False)
+        Should files be overwritten in the target destinations?
+    imext: str (default "png")
+        The file extension of the images in the directory
+    """
+    # Make sure our output directories exist
+    verify_dir(output_root)
+    test_im_dir = os.path.join(output_root, f"test_imgs_{seed}")
+    test_msk_dir = os.path.join(output_root, f"test_masks_{seed}")
+    train_im_dir = os.path.join(output_root, f"train_imgs_{seed}")
+    train_msk_dir = os.path.join(output_root, f"train_masks_{seed}")
+
+    for idir in [test_im_dir, test_msk_dir, train_im_dir, train_msk_dir]:
+        verify_dir(idir)
+        if not is_dir_empty(idir):
+            if not overwrite:
+                print("Output path not empty. Skipping entire operation.")
+                print(idir)
+                return
+            else:
+                clear_dir(idir)
+
+    all_fn = files_of_type(image_dir, "*." + imext)
+    # Calculate how many blanks we can keep
+    ntest = int(test_ratio * len(all_fn))
+
+    # choose files to drop
+    if seed is not None:
+        np.random.seed(seed)
+    test = list(np.random.choice(all_fn, ntest, replace=False))
+    # train = list(filter(lambda x: x not in test, all_fn))  # NOT keeps
+    # assert len(test) == ntest
+    # assert len(test) + len(train) == len(all_fn)
+
+    for f in all_fn:
+        im_file = f
+        msk_file = f.replace(image_dir, mask_dir)
+
+        if im_file in test:
+            out_im = test_im_dir
+            out_msk = test_msk_dir
+        else:
+            out_im = train_im_dir
+            out_msk = train_msk_dir
+
+        shutil.copy(im_file, im_file.replace(image_dir, out_im))
+        shutil.copy(msk_file, msk_file.replace(mask_dir, out_msk))
