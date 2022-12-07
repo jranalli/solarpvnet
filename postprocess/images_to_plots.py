cd053d07591c86f983f43eb87c29d03f7db5b5a2
Joe Ranalli <jar339@psu.edu>
2022-12-07 17:04:33 -0500

    Updated plot creation

--- postprocess/images_to_plots.py
+++ postprocess/images_to_plots.py
@@ -6,18 +6,29 @@ from matplotlib.lines import Line2D
 import numpy as np
 from PIL import Image, ImageFilter, ImageColor
 
+from utils.fileio import verify_dir
 
-def model_boundary_plot(im_dir, truth_dir, pred_dirs, out_dir, model_names=["CA-F", "CA-S", "FR-I", "FR-G", "DE-G", "NY-Q"], colors=["#0000ff", "#000088", "#00ff00", "#008800", "#ff00ff", "#ff8800"]):
+import importlib.util
+
+
+def model_boundary_plot(im_dir, truth_dir, pred_dirs, out_dir, dpi=300, verbose=True, model_names=["CA-F", "CA-S", "FR-I", "FR-G", "DE-G", "NY-Q"], colors=["#0000ff", "#000088", "#00ff00", "#008800", "#ff00ff", "#ff8800"]):
     img_paths = glob.glob(os.path.join(im_dir, "*.png"))
     imgs = [os.path.basename(img_path) for img_path in img_paths]
     titles = ['img', 'truth', 'predictions']
-    [titles.append(mdl) for mdl in model_names]
 
     figsize = 4
     cols = 3
 
-    for i, img in enumerate(imgs):
-        print(f"{i}/{len(imgs)}")
+    if importlib.util.find_spec("tqdm"):
+        from tqdm import tqdm
+        looper = tqdm(imgs)
+    else:
+        looper = imgs
+
+    for i, img in enumerate(looper):
+        if verbose and not importlib.util.find_spec("tqdm"):
+            print(f"{i}/{len(imgs)}")
+
         fig, axes = plt.subplots(1, cols, figsize=(cols * figsize, figsize))
 
         for axis, title in zip(axes, titles):
@@ -42,10 +53,14 @@ def model_boundary_plot(im_dir, truth_dir, pred_dirs, out_dir, model_names=["CA-
         legend_entry = Line2D([0], [0], label="Truth", color="#ff0000")
         handles.append(legend_entry)
 
+        truth_dat.close()
+        img_dat.close()
+
         # Plot prediction borders
         for pred_dir, model, color in zip(pred_dirs, model_names, colors):
-            msk = Image.open(os.path.join(pred_dir, img))
-            msk = msk.resize(img_dat.size)
+            msk_im = Image.open(os.path.join(pred_dir, img))
+            msk = msk_im.resize(img_dat.size)
+            msk_im.close()
 
             msk = binarize(msk, 50)
             brd = bkg_to_alpha(
@@ -55,14 +70,23 @@ def model_boundary_plot(im_dir, truth_dir, pred_dirs, out_dir, model_names=["CA-
             axes[2].imshow(brd)
             legend_entry = Line2D([0], [0], label=model, color=color)
             handles.append(legend_entry)
-        plt.legend(handles=handles, bbox_to_anchor=(1,1), loc="upper left")
-        fig.savefig(os.path.join(out_dir, img), dpi=300)
+        plt.legend(handles=handles, bbox_to_anchor=(1, 1), loc="upper left")
+        verify_dir(out_dir)
+        fig.savefig(os.path.join(out_dir, img), dpi=dpi)
         # plt.show()
         plt.close(fig)
 
 
 
-def multimodel_plot(im_src, truth_dir, pred_dirs, out_dir, model_names=["CA-F", "CA-S", "FR-I", "FR-G", "DE-G", "NY-Q"]):
+
+
+
+def multimodel_plot(im_src, truth_dir, pred_dirs, out_dir, dpi=300, verbose=True, model_names=["CA-F", "CA-S", "FR-I", "FR-G", "DE-G", "NY-Q"]):
+
+    bkg_alpha = 0.5
+    fg_alpha = 0.5
+    overlay_color = "#ff0000"
+
     img_paths = glob.glob(os.path.join(im_src, "*.png"))
     imgs = [os.path.basename(img_path) for img_path in img_paths]
 
@@ -72,35 +96,43 @@ def multimodel_plot(im_src, truth_dir, pred_dirs, out_dir, model_names=["CA-F", 
     figsize = 7
     cols = len(titles)
 
-    for img in imgs:
+    if importlib.util.find_spec("tqdm"):
+        from tqdm import tqdm
+        looper = tqdm(imgs)
+    else:
+        looper = imgs
+
+    for img in looper:
+        if verbose and not importlib.util.find_spec("tqdm"):
+            print(f"{i}/{len(imgs)}")
 
         fig, axes = plt.subplots(1, cols, figsize=(cols * figsize, figsize))
         for axis, title in zip(axes, titles):
             axis.set_axis_off()
             axis.set_title(title, fontsize=15)
 
-        tiles = []
         img_dat = Image.open(os.path.join(im_src, img))
         truth_dat = Image.open(os.path.join(truth_dir, img))
 
-        tiles.append(img_dat)
-        tiles.append(truth_dat)
+        axes[0].imshow(img_dat)
+        axes[1].imshow(img_dat, alpha=bkg_alpha)
+        axes[1].imshow(bkg_to_alpha(mask_colorize(truth_dat, overlay_color)), alpha=fg_alpha)
 
-        for pred_dir in pred_dirs:
-            msk = Image.open(os.path.join(pred_dir, img))
-            msk = msk.resize(img_dat.size)
-            tiles.append(msk)
+        for i, pred_dir in enumerate(pred_dirs):
+            msk_im = Image.open(os.path.join(pred_dir, img))
+            msk = msk_im.resize(img_dat.size)
+            msk_im.close()
 
-        for i, (axis, tile) in enumerate(zip(axes, tiles)):
-            if i==0:
-                axis.imshow(tile)
-            else:
-                tile = tile.convert("P")
-                axis.imshow(tiles[0], alpha=0.5)
-                axis.imshow(mask_to_red(np.asarray(tile)), alpha=0.5)
-                tile.close()
-        tiles[0].close()
-        fig.savefig(os.path.join(out_dir, img))
+            # Binarize, colorize and remove bkg
+            msk = binarize(msk, 50)
+            msk = bkg_to_alpha(
+                mask_colorize(msk, color=overlay_color))
+
+            axes[i+2].imshow(img_dat, alpha=bkg_alpha)
+            axes[i+2].imshow(msk, alpha=fg_alpha)
+
+        verify_dir(out_dir)
+        fig.savefig(os.path.join(out_dir, img), dpi=dpi)
         plt.close(fig)
 
 
@@ -112,23 +144,9 @@ def mask_to_boundary(mask, size=3, color=[255, 255, 255]):
     return Image.fromarray(dif)
 
 
-def mask_to_red(mask, color=[255, 0, 0]):
-    """
-    Converts binary segmentation mask from white to red color.
-    Also adds alpha channel to make black background transparent.
-    """
-    img_size = mask.shape[0]
-    gray = mask.reshape(img_size, img_size)
-    c1 = color[0] * gray
-    c2 = color[1] * gray
-    c3 = color[2] * gray
-    c4 = mask.reshape(img_size, img_size)
-    return np.stack((c1, c2, c3, c4), axis=-1)
-
-
 def mask_colorize(mask, color=[255, 0, 0]):
     if isinstance(mask, Image.Image):
-        arr = np.asarray(mask)
+        arr = np.asarray(mask.convert("RGB"))
     else:
         arr = mask
 
@@ -197,14 +215,23 @@ def demo_manual():
 
 
 if __name__ == "__main__":
-    for model in ["NYC"]:
+    model_names = ["CA-F", "CA-S", "FR-I", "FR-G", "DE-G", "NY-Q"]
+
+    for model in ["Cal_Fresno", "Cal_Stockton", "France_ign", "France_google", "Germany", "NYC"]:
         img_path = fr"d:\solardnn\{model}\tile_subsets\set0_seed42\test_img_42"
         truth_path = fr"d:\solardnn\{model}\tile_subsets\set0_seed42\test_mask_42"
         pred_dirs = [fr"D:\solardnn\results\results_set0_{model}_predbyCal_Fresno_resnet34_42\pred",
                      fr"D:\solardnn\results\results_set0_{model}_predbyCal_Stockton_resnet34_42\pred",
-                     fr"D:\solardnn\results\results_set0_{model}_predbyFrance_google_resnet34_42\pred",
                      fr"D:\solardnn\results\results_set0_{model}_predbyFrance_ign_resnet34_42\pred",
+                     fr"D:\solardnn\results\results_set0_{model}_predbyFrance_google_resnet34_42\pred",
                      fr"D:\solardnn\results\results_set0_{model}_predbyGermany_resnet34_42\pred",
                      fr"D:\solardnn\results\results_set0_{model}_predbyNYC_resnet34_42\pred"]
+        outdir = fr"D:\solardnn\results\border_plots\{model}"
+
+        print(f"\n=={model} BORDER==")
+        model_boundary_plot(img_path, truth_path, pred_dirs, outdir, model_names=model_names)
+
         outdir = fr"D:\solardnn\results\combo_plots\{model}"
-        model_boundary_plot(img_path, truth_path, pred_dirs, outdir)
+
+        print(f"\n=={model} COMBO==")
+        multimodel_plot(img_path, truth_path, pred_dirs, outdir, model_names=model_names)
