9a6a71d64cfb08588c6a93c817d7dd99225ffd09
Joe Ranalli <jar339@psu.edu>
2023-01-04 19:18:26 -0500

    Add fix to cleanup germany red mask images

--- utils/simplify_masks.py
+++ utils/simplify_masks.py
@@ -4,7 +4,7 @@ import numpy as np
 from utils.fileio import files_of_type
 
 
-
+# Utility for normalizing all the images to the same scaling and color.
 
 def process_directory(mydir):
 
@@ -23,6 +23,16 @@ def process_directory(mydir):
 
 
 if __name__=="__main__":
-    path = r"F:\solardnn\Germany\tile_subsets\set0_seed42\train_mask_42"
+    for site in ["Germany", "Cal_Fresno", "Cal_Stockton", "France_ign", "France_google", "NYC", "combo_dataset"]:
+        path = rf"F:\solardnn\{site}\tile_subsets\set0_seed42\train_mask_42"
+        process_directory(path)
 
+        path = rf"F:\solardnn\{site}\tile_subsets\set0_seed42\test_mask_42"
+        process_directory(path)
+
+    path = rf"F:\solardnn\Germany\tile_subsets\mask_set0_seed42"
+    process_directory(path)
+    path = rf"F:\solardnn\Germany\tile_subsets\mask_setfinal_seed42"
+    process_directory(path)
+    path = rf"F:\solardnn\Germany\tiles\mask"
     process_directory(path)
